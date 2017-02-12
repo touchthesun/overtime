0dd743644e6b1cc3f5039963c93774b53d9c2251
Nathan Parker <WitchDoctor@Nathans-MacBook-Air.local>
2017-02-12 20:39:32 +0545

    Connected users to posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,6 +16,9 @@ describe 'navigate' do
 
 describe 'creation' do
   before do 
+    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+    login_as(user, :scope => :user)
+
     visit new_post_path
   end
 
@@ -29,6 +32,14 @@ describe 'creation' do
       click_on "Save"
 
       expect(page).to have_content('some rationale')
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'User Association'
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq('User Association')
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
