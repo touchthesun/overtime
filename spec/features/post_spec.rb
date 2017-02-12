cd22a6b57e2d0cf09ec34a3e15fdae54abf5e1b0
Nathan Parker <WitchDoctor@Nathans-MacBook-Air.local>
2017-02-12 18:14:48 +0545

    Implemented post creation process

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,6 +11,24 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+  end
 
+
+describe 'creation' do
+  before do 
+    visit new_post_path
+  end
+
+  it 'has a new form that can be reached' do
+    expect(page.status_code).to eq(200)
+   end
+
+   it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'some rationale'
+      click_on "Save"
+
+      expect(page).to have_content('some rationale')
+    end
   end
 end
