a2658afef18882032a8383f18d8a1f92e7ee1e27
Nathan Parker <WitchDoctor@Nathans-MacBook-Air.local>
2017-03-03 16:43:24 +0100

    Edit posts functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,36 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
-  def index
-    @posts = Post.all 
+def index
+  @posts = Post.all 
+end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+  @post.user_id = current_user.id 
+
+    if @post.save
+     redirect_to @post, notice: "Your post was created successfully"
+    else 
+      render :new
+    end
+end
+
+def edit
+  
+end
+
+def update
+  if @post.update(post_params)
+    redirect_to @post, notice: "Your post was created successfully"
+  else
+    render :edit
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id 
-
-      if @post.save
-       redirect_to @post, notice: "Your post was created successfully"
-      else 
-        render :new
-      end
-  end
+end
 
   def show
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,22 @@
 FactoryGirl.define do
-  factory :user do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+
+
+  factory :user, class: "User" do
     first_name 'John'
     last_name 'Snow'
-    email "test@test.com"
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
 
+
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
-    email "admin@user.com"
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
