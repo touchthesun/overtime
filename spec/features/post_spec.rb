28688ebb4400c34f154be14c6c3ee0941c21b81a
Nathan Parker <WitchDoctor@Nathans-MacBook-Air.local>
2017-03-03 13:54:58 +0100

    post table formatting

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+    login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of Posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
 
 describe 'creation' do
   before do 
-    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
-    login_as(user, :scope => :user)
-
     visit new_post_path
   end
 
@@ -28,18 +39,18 @@ describe 'creation' do
 
    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'some rationale'
+      fill_in 'post[rationale]', with: "some rationale"
       click_on "Save"
 
-      expect(page).to have_content('some rationale')
+      expect(page).to have_content("some rationale")
     end
 
     it 'will have a user associated with it' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'User Association'
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
-      expect(User.last.posts.last.rationale).to eq('User Association')
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
