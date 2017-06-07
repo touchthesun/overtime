901bad9c3abb1137027a528bcf0b61fb2d4acf2f
Nathan <touchthesun@gmail.com>
2017-06-07 19:07:01 +0200

    integrated application helper for status labels for audit logs

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'pundit', '~> 1.1'
 gem 'puma', '>= 3.8.2'
 gem 'twilio-ruby', '~> 4.13'
 gem 'dotenv-rails', :groups => [:development, :test]
+gem 'kaminari'
 
 
 group :development, :test do
--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,6 +1,8 @@
 class AuditLog < ApplicationRecord
   belongs_to :user, optional: true
 
+  enum status: { pending: 0, confirmed: 1}
+
   validates_presence_of :user_id, :status, :start_date
 
   after_initialize :set_defaults
